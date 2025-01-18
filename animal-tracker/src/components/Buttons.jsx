1a1772b3d448d876de1a08b429903ef1b2b4916a
Miguel Closa <jmrclosa@gmail.com>
2025-01-18 10:55:20 -0500

    Rename: buttons.jsx -> Buttons.jsx

---
+++ animal-tracker/src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+// Make a sticky navbar, route to different pages.
+
+
+function Buttons(props) {
+    return (
+        <div className="buttons-container">
+            <p>hello </p>
+        </div>
+    );
+}
+
+export default Buttons;
