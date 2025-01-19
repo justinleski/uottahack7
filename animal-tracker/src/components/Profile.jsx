bdc42487b57e31cdcd2534706af1520c6b9d29d4
Miguel Closa <jmrclosa@gmail.com>
2025-01-18 19:40:43 -0500

    Create framework in Profile.jsx.

--- animal-tracker/src/components/Profile.jsx
+++ animal-tracker/src/components/Profile.jsx
@@ -3,11 +3,29 @@ import "./Profile.css";
 
 const Profile = () => {
     return (
-        <>
-            <div className="profile-container">
-                <p>This is where the profile goes</p>
+        <div className="profile-container">
+
+            <div className="profile-top">
+                {/*This <i> tag is a placeholder, change it to the user's avatar*/}
+                <i className="bi bi-circle"></i>
+                <p className="username">Insert User Name</p>
             </div>
-        </>
+
+
+            <div className="profile-midsection">
+                <p className="favourite-animal">Insert Favourite Animal</p>
+            </div>
+
+
+            <div className="profile-bottom">
+                <button>View Friends List</button>
+                <button>View Avatar Shop</button>
+                <button>Change Details...</button>
+
+            </div>
+
+
+        </div>
 
     );
 };
