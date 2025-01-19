38d43d4dade0539fecfa18a816c66815b6717c54
Justin <o0justinsgamez0o@gmail.com>
2025-01-18 22:57:08 -0500

    feat: Custom icons for map

--- animal-tracker/src/components/Map.jsx
+++ animal-tracker/src/components/Map.jsx
@@ -5,32 +5,56 @@ import {
   Marker,
   Popup
 } from 'react-leaflet';
-import "leaflet/dist/leaflet.css"
-import './Map.css'
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import './Map.css';
 
 const Map = () => {
-  const position = [51.505, -0.09]
 
-  const customIcon = L.divIcon({
-    className: "custom-div-icon",
-    html: "<div style='background-color:#c30b82;' class='marker-pin'></div><i class='material-icons'>weekend</i>",
-    iconSize: [30, 42],
-    iconAnchor: [15, 42]
-  });
+  const position = [0, 0];
+
+  // sample data
+  const coordinates = [
+    { lat: 500.505, lng: -0.09, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+    { lat: 51.515, lng: -0.1, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+    { lat: 1.525, lng: -0.08, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+  ];
+
+  const createCustomDivIcon = (imgSrc) => L.divIcon({
+      className: 'custom-marker',
+      html: `<div style="
+      padding: 5px; 
+      width: 40px; 
+      height: 40px; 
+      border-radius: 1rem; 
+      background-color:rgb(255, 255, 255); 
+      display: flex; 
+      flex-direction: column; 
+      align-items: center; 
+      justify-content: center; 
+      color: black;
+      text-align: center;">
+        <img src="${imgSrc}" style="width: 50px; height: 30px; border-radius: 0.5rem; margin-top: 2px;">
+    </div>`,
+      iconSize: [10, 10], // Width and height of the div
+      iconAnchor: [15, 15] // Center the icon
+    });
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={1} scrollWheelZoom={true}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    {coordinates.map(({ lat, lng, imgSrc }, index) => (
+        <Marker
+          key={index}
+          position={[lat, lng]}
+          icon={createCustomDivIcon(imgSrc)}
+        />
+      ))}
   </MapContainer>
-  )
+  );
   
 };
 
