d1440cd12d53b0de89f0738475fb07642545f509
Justin <o0justinsgamez0o@gmail.com>
2025-01-18 22:01:25 -0500

    style: Fix centering for map and styling for button

--- animal-tracker/src/components/Map.jsx
+++ animal-tracker/src/components/Map.jsx
@@ -11,6 +11,13 @@ import './Map.css'
 const Map = () => {
   const position = [51.505, -0.09]
 
+  const customIcon = L.divIcon({
+    className: "custom-div-icon",
+    html: "<div style='background-color:#c30b82;' class='marker-pin'></div><i class='material-icons'>weekend</i>",
+    iconSize: [30, 42],
+    iconAnchor: [15, 42]
+  });
+
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
     <TileLayer
