224e25c344676903ebd2023082bccb8de11ba9c6
Justin <o0justinsgamez0o@gmail.com>
2025-01-18 22:57:14 -0500

    feat: Custom icons for map

--- animal-tracker/src/components/Map.jsx
+++ animal-tracker/src/components/Map.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {
   MapContainer,
   TileLayer,
@@ -11,12 +12,14 @@ import './Map.css';
 
 const Map = () => {
 
+  // to position the map
   const position = [0, 0];
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // sample data
   const coordinates = [
     { lat: 500.505, lng: -0.09, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-    { lat: 51.515, lng: -0.1, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+    { lat: 51.515, lng: -0.1, imgSrc: 'https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
     { lat: 1.525, lng: -0.08, imgSrc: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
   ];
 
@@ -25,8 +28,8 @@ const Map = () => {
       html: `<div style="
       padding: 5px; 
       width: 40px; 
-      height: 40px; 
-      border-radius: 1rem; 
+      height: 60px; 
+      border-radius: 0.5rem; 
       background-color:rgb(255, 255, 255); 
       display: flex; 
       flex-direction: column; 
@@ -34,26 +37,37 @@ const Map = () => {
       justify-content: center; 
       color: black;
       text-align: center;">
-        <img src="${imgSrc}" style="width: 50px; height: 30px; border-radius: 0.5rem; margin-top: 2px;">
+        <img src="${imgSrc}" style="width: 100%; height: 100%; border-radius: 0.5rem; object-fit: cover;">
     </div>`,
-      iconSize: [10, 10], // Width and height of the div
-      iconAnchor: [15, 15] // Center the icon
+      iconSize: [15, 15], // Width and height of the div
+      iconAnchor: [10, 20] // Center the icon
     });
 
-  return (
-    <MapContainer center={position} zoom={1} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {coordinates.map(({ lat, lng, imgSrc }, index) => (
-        <Marker
-          key={index}
-          position={[lat, lng]}
-          icon={createCustomDivIcon(imgSrc)}
-        />
-      ))}
-  </MapContainer>
+    const handleMarkerClick = (data) => {
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);
+    };
+
+    return (
+      <MapContainer center={position} zoom={1} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {coordinates.map(({ lat, lng, imgSrc }, index) => (
+          <Marker
+            key={index}
+            position={[lat, lng]}
+            icon={createCustomDivIcon(imgSrc)}
+            eventHandlers={{
+              click: () => handleMarkerClick({ imgSrc }),
+            }}
+          />
+        ))}
+    </MapContainer>
   );
   
 };
